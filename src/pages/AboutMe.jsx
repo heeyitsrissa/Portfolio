a5270c1d7eb3f918b8b59b9d3a30a6a877aced2a
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-05-06 20:33:43 -0400

    added files for navbar, and added pseudo code

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+    // THEN I see a recent photo or avatar of the developer and a short bio about them
+    // WHEN I am presented with the Portfolio section
+    return (
+        <div className="container pt-4">
+            <p>
+
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,1 @@
+// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
